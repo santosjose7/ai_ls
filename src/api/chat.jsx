9e099ba924b6d2e8d301a626c9b3a500c372f09e
Joseph Mokua <mokuaj42@gmail.com>
2025-08-02 09:30:02 +0300

    delete

--- src/api/chat.jsx
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const askQuestion = async (question) => {
-  const res = await axios.post('/api/chat', { question });
-  return res.data;
-};
--- src/api/courses.jsx
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const getCourses = async () => {
-  const response = await axios.get('/api/courses');
-  return response.data;
-};
