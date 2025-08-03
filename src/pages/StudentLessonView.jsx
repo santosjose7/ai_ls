66d3d805bcfe99c6b31c2d6146716aad6531df17
Joseph Mokua <mokuaj42@gmail.com>
2025-08-03 14:38:00 +0300

    21

--- src/pages/StudentLessonView.jsx
+++ src/pages/StudentLessonView.jsx
@@ -67,7 +67,7 @@ const StudentLessonView = () => {
     },
 
     // Tool to get session context information
-    getSessionContext: async () => {
+    getLessonContext: async () => {
       console.log('Agent requested session context');
       return {
         student_name: studentName || "Student",
